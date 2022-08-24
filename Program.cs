294c12349c2f604bffd45d17f54a19598f230351
margarita s <runlet@inbox.ru>
2022-08-24 20:01:09 +0300

    First program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = new int [10];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = new Random().Next(100,1000);
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+
+}
+Console.WriteLine($"Massive:[{String.Join(",",array)}]");
+Console.WriteLine(count);
